e3cbd85ab0ee7bf23bfac9cd4d6856e5c0081110
bksanda <YOUR-EMAIL-ADDRESS>
2015-05-25 10:27:28 -0700

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-express_learn
+###2014-2015, WDI COURSE LESSON
+####Code Along
+***
+
+
+Express Learn
 =============
 
-learning about express
+####Simple app to Learn about Node Express
+
+* npm install --save express ejs
+* ejs is embeddable javascript
+* what's the difference betweeen <% and <%=?
+  * With equal the value appears on the template view page, without it, it is seen but not executed
+  * This code is executed on the computer. It runs the code and sends the HTML to browser.
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("testing")
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res){
+	res.render('guac', {allergies: "onions"});
+});
+
+app.get("/add/:x/:y", function (req,res) {
+	//res.send("result: " + (parseInt(req.params.x) + parseInt(req.params.y)) );
+	res.render("taco", {sum: parseInt(req.params.x) + parseInt(req.params.y)});
+});
+
+app.get("/multiply/:x/:y", function (req,res) {
+	res.send("result: " + (parseInt(req.params.x) * parseInt(req.params.y)) );
+});
+
+app.get("/hello", function(req,res) {
+	res.send("this is the second page.");
+})
+
+app.listen(3000);
